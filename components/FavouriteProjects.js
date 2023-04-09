5ddf4021e91cb7112b7f1ed4fdfd3a4c61c6b1bc
Taha Ababou <taha@beloc.al>
2023-04-09 04:45:15 -0400

    Quicklang link

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -75,7 +75,7 @@ export default function FavouriteProjects() {
           </a>
           {/* Single card */}
           <a
-            href="https://inkognito.tech"
+            href="https://github.com/tahababou12/QuickLang"
             className="w-full block col-span-3 sm:col-span-1  object-cover"
           >
             <div className="relative overflow-hidden shadow-2xl">
