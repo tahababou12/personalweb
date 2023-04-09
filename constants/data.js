6743f483a5b03e2d1c809517cafc462d90252700
Taha Ababou <taha@beloc.al>
2023-04-09 03:59:44 -0400

    Content update

---
+++ constants/data.js
@@ -0,0 +1,123 @@
+const userData = {
+  githubUsername: "tahababou12",
+  name: "Taha H. Ababou",
+  designation: "Software Engineer",
+  avatarUrl: "/official.jpg",
+  email: "tahababou12@gmail.com",
+  phone: "+1 (617) 852-5026",
+  address: "Boston, MA, USA",
+  projects: [
+    {
+      title: "VirtualMe - Personal Chatbot (w/ GPT-4)",
+      link: "https://taha.d3p9cjveerhpg.amplifyapp.com/",
+      imgUrl: "/virtualme3.png",
+    },
+    {
+      title: "RECOG - Image Recognition for Blind Users",
+      link: "https://github.com/tahababou12/Object-Recognizer",
+      imgUrl: "/recog.png",
+    },
+    {
+      title: "Spock",
+      link: "https://github.com/AbhinavMir/spock",
+      imgUrl: "/spock.png",
+    },
+    {
+      title: "Quicklang - Language Learning App",
+      link: "https://github.com/tahababou12/QuickLang",
+      imgUrl: "/quicklang.png",
+    },
+    {
+      title: "CovidRescue - C++ Game",
+      link: "https://github.com/tahababou12/BU/tree/COVID-Outbreak-Escape-the-Fall-Semester-Game",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Matlab Truss Design Simulator",
+      link: "https://github.com/tahababou12/BU/tree/MATLAB-Truss-Design-Simulator",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Personal Portfolio Website 1.0",
+      link: "https://github.com/tahababou12/inkognito",
+      imgUrl: "/inkognito.png",
+    },
+    {
+      title: "Quantum Levitation Research Project",
+      link: "https://www.researchgate.net/publication/342878087_Quantum_Levitation_-_Flying_vehicles_in_the_near_future",
+      imgUrl: "/quantum.jpeg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `Once upon a time, I was just an 11-year-old kid with big dreams and an insatiable curiosity for technology. Little did I know, this would be the start of an exhilarating journey to becoming the innovative software engineer I am today.`,
+      `As I ventured into the mystical realm of computer engineering at Boston University, I tamed the wildest subjects like algorithms and operating systems, crafting a diverse set of programming skills. From Junior Technical Lead at The Society of Scent to Senior Technical Lead at Be Local, I wielded my powers to drive growth and user engagement, managing multicultural teams and optimizing AWS resources with ease.`,
+      `Away from the world of code, I am a true adventurer, conquering golf courses, riding the waves, and capturing breathtaking moments through my lens. This is my story, and it's only just begun!`,
+    ],
+    currentProject: "VirtualMe 👨‍💻",
+    currentProjectUrl: "https://taha.d3p9cjveerhpg.amplifyapp.com/",
+  },
+  experience: [
+    {
+      title: "Senior Technical Lead",
+      company: "Be Local",
+      year: "2022-Present",
+      companyLink: "https://belocal.com",
+      desc: "Led a team of 15 developers, developed and implemented a comprehensive website and app development roadmap, leveraging advanced technologies like Node.js, React Native, Vue.js, AWS, Swift, and Kotlin. Oversaw the successful launch of the company's mobile platform.",
+      },
+      
+      {
+      title: "Junior Technical Lead",
+      company: "The Society of Scent",
+      year: "2022",
+      companyLink: "https://thesocietyofscent.com",
+      desc: "Developed and implemented a hybrid e-commerce mobile app, increasing online sales by 25% and user engagement by 50%. Utilized Angular and Node.js to design and implement software systems and custom components, improving productivity and efficiency. Reduced development time by 50%.",
+      },
+      
+      {
+      title: "Software Engineer",
+      company: "Fondation Ababou",
+      year: "2020",
+      companyLink: "https://fondationababou.com",
+      desc: "Implemented a cloud computing platform, saving the organization $50k. Configured and maintained a secure network infrastructure and comprehensive database using MySQL. Improved data security and availability with redundant servers and backup systems, achieving 99.9% uptime.",
+      },
+      
+      {
+      title: "Teaching Assistant",
+      company: "Sotogrande International School",
+      year: "2019-2020",
+      companyLink: "https://sis.ac",
+      desc: "Taught computer science fundamentals to high-school students, created individualized tutoring plans, and inspired students to consider engineering careers. Demonstrated deep understanding of CS concepts through interactive and engaging lesson plans.",
+      },
+      
+      {
+      title: "Full Stack Web Developer",
+      company: "Fondation Ababou",
+      year: "2019-2020",
+      companyLink: "https://fondationababou.com",
+      desc: "Developed QuickLang, a language-learning web platform for French speakers in rural areas. Utilized PHP, HTML, JavaScript, and MySQL to create an interactive and engaging web app, impacting the lives of young people from disadvantaged backgrounds.",
+      },
+      
+      {
+      title: "Associate Producer",
+      company: "Black 33 Media Limited",
+      year: "2018",
+      companyLink: "https://black33media.com",
+      desc: "Produced a business marketing video using Final Cut Pro X, created a custom soundtrack with Logic Pro X, and learned to operate professional cinema drones. Improved collaboration and creative skills while gaining valuable experience in the film industry.",
+      }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1C-jI1boXMFCKJiVcWqC09FgrhpOJBTSw/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/tahababou12",
+    twitter: "https://twitter.com/",
+    linkedin: "https://linkedin.com/in/tahaababou",
+    github: "https://github.com/tahababou12",
+    facebook: "https://facebook.com/",
+    peerlist: "https://peerlist.io/",
+  },
+};
+
+export default userData;
---
+++ pages/experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Experience from "../components/Experience";
+
+export default function experience() {
+  return (
+    <ContainerBlock title="Experience - Taha H. Ababou">
+      <Experience />
+    </ContainerBlock>
+  );
+}
