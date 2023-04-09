bb25fca0c7c2f80e5b61e305eaa0058cc893afd7
Taha Ababou <taha@beloc.al>
2023-04-09 04:23:57 -0400

    RECOG link update

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -55,7 +55,7 @@ export default function FavouriteProjects() {
           </a>
           {/* Single card */}
           <a
-            href="https://placeholdertech.in"
+            href="https://github.com/tahababou12/Object-Recognizer"
             className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
           >
             <div className="relative overflow-hidden">
